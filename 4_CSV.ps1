6b83f27bf3a6592ec151e02844a556d1ca40b98a
learningitio <philip@learning-it.io>
2020-08-19 13:27:25 +0200

    com

--- 4_CSV.ps1
+++ 4_CSV.ps1
@@ -9,8 +9,13 @@ $eigenesArray += $object
 
 # CSV Export
 $eigenesArray | export-csv -path C:\test.csv -delimiter ";"
-get-process | export-csv -path C:\test2.csv -delimiter ";"
+get-childitem | export-csv -path C:\test2.csv -delimiter ";"
 
 
 # CSV Import
 $csv = import-csv -path C:\test2.csv -delimiter ";"
+$csv.Modules
+
+#Filter
+$csv = import-csv -path C:\test2.csv -delimiter ";" | sort Name
+$csv = import-csv -path C:\test2.csv -delimiter ";" | where {$_.HandleCount -gt 10}
