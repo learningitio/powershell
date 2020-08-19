099aaabc24e0e8ded1c0a29e2425ab2a52fc7684
learningitio <philip@learning-it.io>
2020-08-19 12:37:30 +0200

    com

--- 1_powershell_passwords.ps1
+++ 1_powershell_passwords.ps1
@@ -6,3 +6,25 @@ $pass = Read-Host "Enter Password" -AsSecureString | ConvertFrom-SecureString | 
 
 # Import und Convert2Secure
 $pass = Get-Content "C:\pass.txt" | ConvertTo-SecureString
+
+# Generieren CredentialObject
+$user = Read-Host "Enter Username"
+$File = "C:\pass.txt"
+$MyCredential = New-Object -TypeName System.Management.Automation.PSCredential `
+ -ArgumentList $User, (Get-Content $File | ConvertTo-SecureString)
+
+# ABER
+$password = ConvertTo-SecureString 'P@ssw0rd' -AsPlainText -Force
+$Ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($password)
+$result 
+
+
+
+# Methode 2 - Powershell SecretManagement
+Install-Module -Name Microsoft.PowerShell.SecretsManagement -AllowPrerelease
+
+Get-SecretVault
+Get-SecretInfo
+
+Set-Secret -Name "NAME" -SecureStringSecret "SECRET"
+Get-Secret -Name "NAME"
