608d8cc2b31b364e4878c1162ff5be5bbafa7e8b
learningitio <philip@learning-it.io>
2020-08-19 13:32:41 +0200

    com

---
+++ 5_string_pasrsing.ps1
@@ -0,0 +1,15 @@
+$string = "Ich heiße Philip und betreibe LearningIT"
+
+# Split
+$string.split(" ")
+$array = $string.split(" ")
+$anfangswert = $array[0]
+
+# Replace
+$string.Replace("Philip", "Tom")
+
+# Remove
+$string.Remove(1,5)
+
+# Lenght
+$string.Lenght
