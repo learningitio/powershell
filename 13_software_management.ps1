28182a97e9e9c9efe5f1a60c05b2f952d4f4663f
learningitio <philip@learning-it.io>
2020-08-20 21:15:52 +0200

    com

---
+++ 13_software_management.ps1
@@ -0,0 +1,15 @@
+# Installierte Programme auslesen
+Get-CimInstance -class Win32_Product 
+Get-CimInstance -class Win32_Product -Property *
+
+# Install
+Start-Process "C:\vlc-3.0.4-win64.msi" -ArgumentList '/quiet' -Wait
+
+# Uninstall
+Get-CimInstance -Class Win32_Product | Where-Object {$_.Name -like "*VLC*"} | Invoke-CimMethod -MethodName Uninstall
+
+# Install CIM
+Invoke-CimMethod -Class Win32_Product -MethodName Install -Arguments @{PackageLocation='C:\vlc-3.0.4-win64.msi'}
+
+# Upgrade
+Invoke-CimMethod -Class Win32_Product -MethodName Upgrade -Arguments @{PackageLocation='C:\vlc-3.0.4-win64.msi'}
