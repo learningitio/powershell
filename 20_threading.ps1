a0cbf9cf055651785f208a19a5f1bffe5363a682
learningitio <philip@learning-it.io>
2020-08-20 22:19:56 +0200

    com

---
+++ 20_threading.ps1
@@ -0,0 +1,28 @@
+# Job starten
+start-job -scriptblock {
+    write-host "hello"
+    start-sleep -seconds 10
+}
+
+# Alternativ für kurze Tasks (zB API-Call)
+get-eventlog -Logname System -Job
+
+# Jobs auflisten
+get-job
+
+# Job-Output auslesen
+receive-job 2
+
+# -> Jobs im Hintergrund laufen lassen, um zu einem späteren Zeitpunk den Output abzufragen
+
+
+
+# Multithreading mit PS7 für ForEach
+$ip = @()
+$ip += "8.8.8.8"
+$ip += "8.8.4.4"
+$ip += "localhost"
+
+$ip | foreach-object -parallel  {
+    test-connection $_
+} # -throttlelimit 2
