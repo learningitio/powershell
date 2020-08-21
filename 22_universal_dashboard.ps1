cfd5df44f8360b6538806b57eb22c2f52f2b980e
learningitio <philip@learning-it.io>
2020-08-21 17:33:01 +0200

    com

--- 22_universal_dashboard.ps1
+++ 22_universal_dashboard.ps1
@@ -2,10 +2,13 @@ Install-Module UniversalDashboard.Community -AcceptLicense
 
 # https://dev.to/jcoelho/powershell-universal-dashboard-making-interactive-dashboards-9kl
 
+# https://docs.universaldashboard.io/getting-started
 
-$theme = Get-UDTheme -Name 'Azure'
-$dashboard = New-UDDashboard -Title "LearningIT" -theme $theme -Content{
-    New-UDHeading -Text "Powershell is Fun!!"
-}
 
-Start-UDDashboard -Dashboard $dashboard -Port 1000 -AutoReloadY
+# First Start
+
+# Theme change
+
+# Monitoring
+
+# Charts
