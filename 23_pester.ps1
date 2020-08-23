73d17aa6225722a2c66c2bba9f9046b2995d9f55
learningitio <philip@learning-it.io>
2020-08-23 18:15:30 +0200

    com

--- 23_pester.ps1
+++ 23_pester.ps1
@@ -3,7 +3,29 @@ Install-Module Pester -Force -SkipPublisherCheck
 
 # Grundgedanke -> Test-Driven Development...erst Tests, dann Funktionalität
 
-function TestPester {
-    write-host "test"    
+
+Import-Module Pester
+
+#Sample function to run tests against    
+function Add-Numbers{
+    param($a, $b)
+    return [int]$a + [int]$b
 }
 
+#Group of tests
+Describe "Validate Add-Numbers" {
+
+        #Individual test cases
+        It "Should add 2 + 2 to equal 4" {
+            Add-Numbers 2 2 | Should Be 4
+        }
+
+        It "Should handle strings" {
+            Add-Numbers "2" "2" | Should Be 4
+        }
+
+        It "Should return an integer"{
+            Add-Numbers 2.3 2 | Should BeOfType Int32
+        }
+
+}
