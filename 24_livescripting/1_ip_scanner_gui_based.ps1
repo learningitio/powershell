de7890ad6df25a6988a515cd808e8aa1df765439
learningitio <philip@learning-it.io>
2020-08-25 18:01:28 +0200

    com

--- 24_livescripting/1_ip_scanner_gui_based.ps1
+++ 24_livescripting/1_ip_scanner_gui_based.ps1
@@ -7,7 +7,7 @@ Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $network_scanner                 = New-Object system.Windows.Forms.Form
-$network_scanner.ClientSize      = New-Object System.Drawing.Point(365,418)
+$network_scanner.ClientSize      = New-Object System.Drawing.Point(291,422)
 $network_scanner.text            = "Network Scanner"
 $network_scanner.TopMost         = $false
 
@@ -18,31 +18,73 @@ $TextBox1.height                 = 20
 $TextBox1.location               = New-Object System.Drawing.Point(9,33)
 $TextBox1.Font                   = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$ComboBox1                       = New-Object system.Windows.Forms.ComboBox
-$ComboBox1.text                  = "  SN"
-$ComboBox1.width                 = 56
-$ComboBox1.height                = 20
-@('/8','/16','/32') | ForEach-Object {[void] $ComboBox1.Items.Add($_)}
-$ComboBox1.location              = New-Object System.Drawing.Point(146,33)
-$ComboBox1.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
-
 $GetactiveIPs                    = New-Object system.Windows.Forms.Button
 $GetactiveIPs.text               = "Get Active IPs"
 $GetactiveIPs.width              = 118
 $GetactiveIPs.height             = 30
-$GetactiveIPs.location           = New-Object System.Drawing.Point(216,26)
+$GetactiveIPs.location           = New-Object System.Drawing.Point(139,27)
 $GetactiveIPs.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$GetactiveIPs.add_Click({PingAndGridOut $TextBox1.Text})
 
 $DataGridView1                   = New-Object system.Windows.Forms.DataGridView
-$DataGridView1.width             = 348
+$DataGridView1.width             = 267
 $DataGridView1.height            = 337
-$DataGridView1.location          = New-Object System.Drawing.Point(8,67)
+$DataGridView1.location          = New-Object System.Drawing.Point(6,68)
 
-$network_scanner.controls.AddRange(@($TextBox1,$ComboBox1,$GetactiveIPs,$DataGridView1))
+$Debugging                       = New-Object system.Windows.Forms.TextBox
+$Debugging.multiline             = $false
+$Debugging.width                 = 118
+$Debugging.height                = 20
+$Debugging.location              = New-Object System.Drawing.Point(9,8)
+$Debugging.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$network_scanner.controls.AddRange(@($TextBox1,$GetactiveIPs,$DataGridView1,$Debugging))
 
 
 
 
 #Write your logic code here
+
+[void]$network_scanner.ShowDialog()
+
+
+
+
+
+#Write your logic code here
+
+function PingAndGridOut {
+    param (
+        [string]$IP
+    )
+
+    $ip_formatted = $IP.Split(".")
+    $okt1 = $ip_formatted[0]
+    $okt2 = $ip_formatted[1]
+    $okt3 = $ip_formatted[2]
+
+    for ($i = 1; $i -lt 255; $i++){
+
+        $ip = $okt1 + "." + $okt2 + "." + $okt3 + "." + $i
+        $DataGridView1.Rows.Add($ip)
+        <#
+                if (Test-Connection $ip){
+
+            $DataGridView1.Rows.Add($ip)
+
+        }
+        #>
+
+        
+
+    }
+
+
+
+    $Debugging.Text = $ip_formatted[0]
+    
+}
+
+
 
 [void]$network_scanner.ShowDialog()
