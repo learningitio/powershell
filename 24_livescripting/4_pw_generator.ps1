376cd34b84f5acd5b4eb441b0190e7008dea3f5e
learningitio <philip@learning-it.io>
2020-08-26 16:29:31 +0200

    com

---
+++ 24_livescripting/4_pw_generator.ps1
@@ -0,0 +1,17 @@
+function Get-RandomCharacters($length, $characters) {
+    $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
+    $private:ofs= ""
+    return [String]$characters[$random]
+}
+
+ 
+$password = Get-RandomCharacters -length 5 -characters 'abcdefghiklmnoprstuvwxyz'
+$password += Get-RandomCharacters -length 1 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
+$password += Get-RandomCharacters -length 1 -characters '1234567890'
+$password += Get-RandomCharacters -length 1 -characters '!"§$%&/()=?}][{@#*+'
+ 
+Write-Host $password
+ 
+$password = Scramble-String $password
+ 
+Write-Host $password
